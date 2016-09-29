d479b160f6506dbf3dea6d2c29f184baa4c155ac
shawn <jpxx@umich.edu>
2016-09-28 23:39:36 -0400

    Bucket Sort using JavaScript

---
+++ sorting-algorithm/bucketSort.js
@@ -0,0 +1,38 @@
+function bucketSort(arr, bucketSize) {
+    if (arr.length === 0) {
+      return arr;
+    }
+
+    var i;
+    var minValue = arr[0];
+    var maxValue = arr[0];
+    for (i = 1; i < arr.length; i++) {
+      if (arr[i] < minValue) {
+          minValue = arr[i];
+      } else if (arr[i] > maxValue) {
+          maxValue = arr[i];
+      }
+    }
+
+    var DEFAULT_BUCKET_SIZE = 5;
+    bucketSize = bucketSize || DEFAULT_BUCKET_SIZE;
+    var bucketCount = Math.floor((maxValue - minValue) / bucketSize) + 1;
+    var buckets = new Array(bucketCount);
+    for (i = 0; i < buckets.length; i++) {
+        buckets[i] = [];
+    }
+
+    for (i = 0; i < arr.length; i++) {
+        buckets[Math.floor((arr[i] - minValue) / bucketSize)].push(arr[i]);
+    }
+
+    arr.length = 0;
+    for (i = 0; i < buckets.length; i++) {
+        insertionSort(buckets[i]);
+        for (var j = 0; j < buckets[i].length; j++) {
+            arr.push(buckets[i][j]);
+        }
+    }
+
+    return arr;
+}
